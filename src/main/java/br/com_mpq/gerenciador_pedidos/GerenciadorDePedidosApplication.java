45643ab4a38ceb66efeb68bce3c2daf4971cfa87
Matheus Quirino <contato.matheuspquirino@gmail.com>
2025-12-09 17:14:06 -0300

    Basic structure for Pizzaria Project

---
+++ src/main/java/br/com_mpq/gerenciador_pedidos/GerenciadorDePedidosApplication.java
@@ -0,0 +1,13 @@
+package br.com_mpq.gerenciador_pedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorDePedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorDePedidosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com_mpq/gerenciador_pedidos/GerenciadorDePedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com_mpq.gerenciador_pedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorDePedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
