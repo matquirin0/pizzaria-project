5262d22146b57462d81f7ea1f9f5f85114602632
pedroquirino <pedrohpquirino@gmail.com>
2026-01-06 11:52:55 -0300

    :recycle: refactor(Product): Importação do enum Category.

--- src/main/java/br/com_mpq/gerenciador_pedidos/Entities/Product.java
+++ src/main/java/br/com_mpq/gerenciador_pedidos/Entities/Product.java
@@ -1,5 +1,6 @@
 package br.com_mpq.gerenciador_pedidos.Entities;
 
+import br.com_mpq.gerenciador_pedidos.Entities.enums.Category;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,7 +14,7 @@ import java.math.BigDecimal;
 public class Product {
     private int idProduct;
     private String name;
-    private String category;
+    private Category category;
     private String description;
     private BigDecimal price;
 }
